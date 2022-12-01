e7c867c9481657824ad6d3bddde114376a835adf
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2022-12-01 16:29:09 -0500

    Create FreestyleMethodsBlackjack.py

---
+++ to be sorted/FreestyleMethodsBlackjack.py
@@ -0,0 +1,15 @@
+#this script is going to require some googling: I want you to practice using your resources with this one. But of course if you get stuck, reach out :)
+'''instructions: randomly generate three values between 1 and 11 in main, then send them to your function bust. 
+in the function bust: add these three numbers together. 
+if they add up to or less than 21, return the sum. 
+If it's over 21, return 0. 
+If it's over 21 BUT there's an 11 as one of the values, return the sum - 10. '''
+
+def bust():
+    
+    
+    
+def main():
+    
+
+main()
