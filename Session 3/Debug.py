03f22074942c0fcd5ccab659e244be963beb6d2c
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2022-12-01 16:14:25 -0500

    Create Debug.py

---
+++ Session 3/Debug.py
@@ -0,0 +1,16 @@
+def main():
+    #i'm trying to do math with the numbers 2 and 4, but it's getting 3 and 5... why?
+    numbers = [1, 2, 3, 4, 5]
+    print(numbers[2])
+    print(numbers[4])
+    print(numbers[4] / numbers[2])
+    print(numbers[4] * numbers[2])
+    
+    #again, why is it getting 7 and 20, I wanted 3 and 29!
+    numbers2 = [465, 3, 30, 7, 29, 20, 82, 13, 5]
+    print(numbers2[3])
+    print(numbers2[5])
+    
+    
+    
+main()
