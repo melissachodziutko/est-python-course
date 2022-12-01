19074da73f6d7e61f86c8dfcddc0498d4b97c8ab
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2022-12-01 16:19:04 -0500

    Create Debugifelse.py

---
+++ to be sorted/Debugifelse.py
@@ -0,0 +1,18 @@
+
+
+def main():
+    temp = 5
+    if temp > 5:
+        print("temp is greater than or equal to 5")
+        
+    
+    #enter your name here
+    name = ""
+    '''in this if/elif/else, add 4 mispellings of your name for if/elif comparisons, and then have your last elif be elif name = "your properly spelled name" '''
+    if name == "mispelling here":
+        print("That's not right!")
+    elif name == "another mispelling":
+        print("That's not right!")
+    #etc etc, the else can be whatever you want
+    
+main()
