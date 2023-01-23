1c72db6357b885bbde7f0823b2531d1a8416f239
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2023-01-23 15:55:16 -0500

    Create Debug.py

---
+++ Session 8/Debug.py
@@ -0,0 +1,7 @@
+'''I took these straight off of google, i'll tell you how to change them, but you have to apply your knowledge as to 
+where and what you're changing'''
+
+f = open("demofile2.txt", "a")
+f.write("See you soon!")
+f.close()
+
